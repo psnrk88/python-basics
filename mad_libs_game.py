52fe18fe3974a26a55c5e1ed00aeab86e071fcab
Nata <psnrk88@gmail.com>
2026-04-01 00:02:29 -0400

    mad_libs_game

---
+++ mad_libs_game.py
@@ -0,0 +1,6 @@
+color = input("Enter your color: ")
+plural_noun = input("Enter your plural_noun: ")
+person = input("Enter your person: ")
+print("Roses are " + color)
+print(plural_noun + " are blue")
+print("I love "+person)
